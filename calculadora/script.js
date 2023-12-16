f837b937539f74db0b0b417314d049f80aa20c37
aneperez <127619963+aneperez@users.noreply.github.com>
2023-12-16 17:10:25 -0300

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,51 @@
+function Calcular(){
+    else.preventDefault()
+
+
+
+    let n1 = parseInt(document.getElementById('num1'))
+    let n2 = parseInt(document.getElementById('num2'))
+    let operacao = parseInt(document.getElementById('operacao'))
+    let resultado;
+
+
+    //validação para o usuário não digitar oalavras e apenas números
+    if(NaN(n1)  &&NaN(n2)  ){
+        alert("Somente números")
+
+    }
+
+    switch   (operacao) {
+        case value:
+
+
+        break:
+        case 'Somar':
+
+        break:
+        case value:
+
+        break:
+        case value:
+
+        break:
+        case value:
+
+        break:
+        case value:
+
+        default:
+
+
+            break:
+
+
+
+
+    }
+
+
+
+
+
+}
